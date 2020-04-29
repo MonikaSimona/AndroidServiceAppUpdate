ad2f32c166649672503d47922138976c7684098d
Simona Simonovska <56088273+MonikaSimona@users.noreply.github.com>
2020-04-29 10:45:00 +0200

    no interval -i in ping

--- app/src/main/java/com/example/qoscheckapp/network_task/NetworkPingTask.java
+++ app/src/main/java/com/example/qoscheckapp/network_task/NetworkPingTask.java
@@ -1,6 +1,8 @@
 package com.example.qoscheckapp.network_task;
 
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -10,9 +12,17 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.BufferOverflowException;
 
 public class NetworkPingTask extends AsyncTask<Void,Void,String>{
     public static String ping;
+    public String host;
+    public int count;
+    public int packetSize;
+    public  int jobPeriod;
+    public BufferedReader input;
+    public String line;
+
 
     @Override
     protected String doInBackground(Void... voids) {
@@ -22,18 +32,19 @@ public class NetworkPingTask extends AsyncTask<Void,Void,String>{
             JSONArray jsonArray = new JSONArray(jsonData);
             for(int i=0; i<jsonArray.length(); i++){
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                String host = jsonObject.getString("host");
-                int count = jsonObject.getInt("count");
-                int packetSize = jsonObject.getInt("packetSize");
-                int jobPeriod = jsonObject.getInt("jobPeriod");
+                 host = jsonObject.getString("host");
+                 count = jsonObject.getInt("count");
+                 packetSize = jsonObject.getInt("packetSize");
+//                 jobPeriod = jsonObject.getInt("jobPeriod");
 //                String jobType = jsonObject.getString("jobtType");
 
                 try{
-                    String pingCmd = "ping -i " + jobPeriod + " -s " + packetSize + " -c "+ count + " " + host;
+
+                    String pingCmd = "ping -s " + packetSize + " -c "+ count + " " + host;
                     Runtime runtime = Runtime.getRuntime();
                     Process process = runtime.exec(pingCmd);
-                    BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                    String line ;
+                    input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    line = " ";
                     while ((line = input.readLine()) != null){
                         Log.i("PINGLINE", "doInBackground: " + line);
                         ping += line;
